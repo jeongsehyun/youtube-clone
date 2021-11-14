818a2798421428867ae9c1a60e2fa55b800241fd
sehyun.dev <sehyun.dev@gmail.com>
2021-11-15 01:00:00 +0900

    show detail when click on the thumbnail

--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import styles from "./video_list.module.css";
 import VideoItem from "../video_item/video_item";
 
-function VideoList({ videos }) {
+function VideoList({ videos, onVideoClick, display }) {
   return (
     <ul className={styles.videos}>
       {videos.map((video) => (
-        <VideoItem key={video.id} video={video} />
+        <VideoItem
+          key={video.id}
+          video={video}
+          onVideoClick={onVideoClick}
+          display={display}
+        />
       ))}
     </ul>
   );
